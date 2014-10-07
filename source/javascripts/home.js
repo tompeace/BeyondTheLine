76df64fa6059639f01a3b14329ef8415a22cb785
Peace <Peace@Peace-4.local>
2014-10-07 17:26:43 +0100

    First commit

---
+++ source/javascripts/home.js
@@ -0,0 +1,43 @@
+//= require "modernizr/modernizr.js"
+//= require "jquery"
+//= require "jquery.transit"
+
+// -------------------------------------------------
+//
+// Home
+// 
+// -------------------------------------------------
+
+(function() {
+
+  "use strict";
+
+  var Piece = function() {
+
+    var self = this;
+
+    self.init();
+
+  };
+
+  Piece.prototype = {
+
+    // -------------------------------------------------
+    //
+    // Initial scene setup
+    // 
+    // -------------------------------------------------
+
+    init: function() {
+
+      var self = this;
+
+      console.log('hello world');
+
+    }
+
+  };
+
+  new Piece();
+
+})();
